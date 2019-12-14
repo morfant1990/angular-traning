d28f267d065cb11f1a3e34dd1213e725b9d4186a
Dmitry Potapov <morfant1990@gmail.com>
2019-12-14 17:01:18 +0400

    Commit services components (step 4).

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,20 +10,22 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  hero: Hero = {
-    id: 1,
-    name : 'Windstorm'
-  };
 
-  constructor() { }
+  constructor(private heroService: HeroService) {
+  }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero) {
     this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    this.heroService.getHerouse().subscribe(heroes => this.heroes = heroes)
   }
 }
