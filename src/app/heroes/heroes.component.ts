4b8454e3ce9741171eeba977171e9c73b7760006
Dmitry Potapov <morfant1990@gmail.com>
2019-12-14 19:09:35 +0400

    Commit working with http and search (step 5).

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -22,4 +22,19 @@ export class HeroesComponent implements OnInit {
   getHeroes(): void {
     this.heroService.getHerouse().subscribe(heroes => this.heroes = heroes)
   }
+
+  add(name: string) {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({name} as Hero).subscribe(hero => {
+      this.heroes.push(hero)
+    });
+  }
+
+  delete(hero: Hero) {
+    this.heroes = this.heroes.filter(h => h != hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
